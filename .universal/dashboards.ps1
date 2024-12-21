71cb7d6cd6f813aee38f7c05207e04625f8fa564
Kevin Bjones <kevin.bjones@student.ehb.be>
2024-12-21 21:22:53 +0100

    Working Toggle Button

--- .universal/dashboards.ps1
+++ .universal/dashboards.ps1
@@ -1,1 +1,2 @@
-﻿New-PSUApp -Name "Hyper-V" -FilePath "dashboards\Hyper-V\Hyper-V.ps1" -BaseUrl "/Hyperv" -Environment "PowerShell 7" -Authenticated -AutoDeploy
+﻿New-PSUApp -Name "Hyper-V" -FilePath "dashboards\Hyper-V\Hyper-V.ps1" -BaseUrl "/Hyperv" -Environment "PowerShell 7" -Authenticated -AutoDeploy 
+New-PSUApp -Name "Test" -FilePath "dashboards\Test\Test.ps1" -BaseUrl "/Test" -Authenticated -AutoDeploy
---
+++ dashboards/Test/Test.ps1
@@ -0,0 +1,19 @@
+﻿New-UDApp -Title "Test" -Content {
+$Data = @(
+     @{Dessert = 'Frozen yoghurt'; Calories = 159; Fat = 6.0; Carbs = 24; Protein = 4.0}
+     @{Dessert = 'Ice cream sandwich'; Calories = 159; Fat = 6.0; Carbs = 24; Protein = 4.0}
+     @{Dessert = 'Eclair'; Calories = 159; Fat = 6.0; Carbs = 24; Protein = 4.0}
+     @{Dessert = 'Cupcake'; Calories = 159; Fat = 6.0; Carbs = 24; Protein = 4.0}
+     @{Dessert = 'Gingerbread'; Calories = 159; Fat = 6.0; Carbs = 24; Protein = 4.0}
+ ) 
+ $Columns = @(
+     New-UDTableColumn -Property Dessert -Title Dessert -Render { 
+        New-UDButton -Id "btn$($EventData.Dessert)" -Text "Click for Dessert!" -OnClick { Show-UDToast -Message $EventData.Dessert } 
+     }
+     New-UDTableColumn -Property Calories -Title Calories 
+     New-UDTableColumn -Property Fat -Title Fat 
+     New-UDTableColumn -Property Carbs -Title Carbs 
+     New-UDTableColumn -Property Protein -Title Protein 
+ )
+ New-UDTable -Data $Data -Columns $Columns -Id 'table3'
+}
